8def4a255294c6ec5c6dbebec53e3a85bba9febe
David Carmona <davidcarmona@gmail.com>
2020-04-10 20:00:25 -0600

    adding 03-homework

---
+++ README.md
@@ -0,0 +1,17 @@
+Password Generator
+This web base password generator was designed to give the user the ability to randomly select a password based off their desired criteria; With the limitation of a minimu and maximum length of characters as well as at least one criteria.
+
+Getting Started
+Choose the criteria for your password
+
+Would you like to use symbols?
+Would you like to use numbers?
+Would you to use upper case letters?
+Would you like to use lower case letters?
+How many characters would you like in your password? Must be at least 8 but no more then 128 characters
+
+Built With
+JS
+
+Acknowledgments
+WOOT!
---
+++ script.js
@@ -0,0 +1,151 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Create Variables with arrays
+
+var symbols = [
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "+",
+  "=",
+  "?",
+  "<",
+  ">",
+];
+var num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var upperLet = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var lowerLet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+var chosenCharacters = [];
+
+//Password criteria
+var passSymbol = confirm("Would you like to use symbols?");
+var passNum = confirm("would you like to use numbers?");
+var upperPass = confirm("Would you to use upper case letters?");
+var lowerPass = confirm("Would you like to use lower case letters?");
+var length = prompt(
+  "How many characters would you like in your password? Must be at least 8 but no more then 128 characters"
+);
+console.log(typeof parseInt(length));
+if (typeof parseInt(length) !== "number") {
+  //not working
+  alert("Please use digits or numbers.");
+}
+if (length > 128 || length < 8) {
+  alert("Please select between 8 and 128 characters.");
+}
+
+console.log(length);
+
+var passLength = parseInt(length);
+
+///Conditional statements
+if (passSymbol === true) {
+  chosenCharacters = chosenCharacters.concat(symbols);
+}
+if (passNum === true) {
+  chosenCharacters = chosenCharacters.concat(num);
+}
+
+if (upperPass === true) {
+  chosenCharacters = chosenCharacters.concat(upperLet);
+}
+if (lowerPass === true) {
+  chosenCharacters = chosenCharacters.concat(lowerLet);
+}
+console.log(chosenCharacters);
+
+if (
+  passSymbol === false &&
+  passNum === false &&
+  upperPass === false &&
+  lowerPass === false
+) {
+  alert("Must choose at least one criteria!!!");
+}
+
+/// Function
+
+function randomPass() {
+  var pass = "";
+  //not right
+  for (var i = 0; i < passLength; i++) {
+    pass =
+      pass +
+      chosenCharacters[Math.floor(Math.random() * chosenCharacters.length)];
+  }
+  return pass;
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = randomPass();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
